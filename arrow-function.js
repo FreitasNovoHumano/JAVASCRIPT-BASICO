a6a5433288caacc1d32cf5947f44e018342cd7cc
Fábio Freitas <ecommercefreitas@gmail.com>
2023-06-07 22:29:20 -0300

    Arrow-function

---
+++ arrow-function.js
@@ -0,0 +1,15 @@
+//function
+
+//function soma(a, b) {
+//return a + b;
+//}
+
+//console.log(soma(3, 5)); //8
+
+//Arrow function
+
+const soma = (num1, num2) => num1 + num2; //Após a seta é o return
+
+const sayHello = (name) => `Hello ${name}`;
+
+console.log(sayHello("Freitas"));
